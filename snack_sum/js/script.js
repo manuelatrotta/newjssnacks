dfd3ceae44835fa51683d27741fd09f1ce43a9a7
manuelatrotta <manuelat88@hotmail.it>
2020-05-09 18:10:26 +0200

    sum

---
+++ snack_sum/js/script.js
@@ -0,0 +1,12 @@
+alert('ola');
+
+//somma i numeri presenti nell'array
+
+var array = [1,2,3,4,5,6,7,8,9];
+
+var sum = 0;
+
+for (var i = 0; i < array.length; i++) {
+  sum = sum + array[i];
+  console.log(sum);// somma in sequenza 
+}
